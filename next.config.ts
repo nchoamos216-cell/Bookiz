5649efaab4535f27fb6424df3027767d4184e6ab
Amos N'cho <nchoamos216@gmail.com>
2026-08-09 17:34:12 +0000

    Nettoyage next.config.ts suppression eslint

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // Ignore les erreurs TypeScript pendant le build sur Render
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Ignore les erreurs ESLint pendant le build
-    ignoreDuringBuilds: true,
   },
 };
 

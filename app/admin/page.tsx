b3481e8ab9db83319111e5d4ee15a57142e5cb0e
Amos N'cho <nchoamos216@gmail.com>
2026-08-11 03:32:16 +0000

    Add admin page correctly

---
+++ app/admin/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminPage() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Bookiz - Administration</h1>
+      <p>Bienvenue sur le tableau de bord administrateur !</p>
+    </div>
+  );
+}

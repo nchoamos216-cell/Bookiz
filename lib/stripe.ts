2ca14534ced16452abfedc390d67d5920a8a2ca8
Amos N'cho <nchoamos216@gmail.com>
2026-08-11 12:27:14 +0000

    Remove hardcoded apiVersion to use default stripe version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,5 @@
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  // Soit tu mets une version stable, soit tu supprimes la ligne apiVersion
-  apiVersion: '2025-02-27.acacia', 
   typescript: true,
 })
